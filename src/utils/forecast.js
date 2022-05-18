967b39b850ec2b61eaac2a3c1c76d08d0b8902e9
Urmil Pandey <urmilpandey@Urmils-MacBook-Air.local>
2022-05-19 03:17:07 +0530

    More udpate Weather Info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,9 @@ const forecast = (longitude,latitude,callback)=>{
         }else if(body.error){
            callback('Unable to get the correct response',undefined)
         }else{
-           callback(undefined,'Weather is '+body.current.weather_descriptions[0]+'. Temperature is '+ body.current.temperature+' degree out but it feels like '+ body.current.feelslike + ' degree. Humidity is '+body.current.humidity+ ' and Wind speed is around '+body.current.wind_speed+'km/hr to '+body.current.wind_dir + '. Current UV_Index ' + body.current.uv_index  )
+           callback(undefined,'Weather is '+body.current.weather_descriptions[0]+'. Temperature is '+ body.current.temperature+
+           ' degree out but it feels like '+ body.current.feelslike + ' degree. Humidity is '+body.current.humidity+ 
+           '% and Wind speed is around '+body.current.wind_speed+' km/hr to '+body.current.wind_dir + '. Current UV_Index ' + body.current.uv_index  )
         }
 
     })
